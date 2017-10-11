0cfc6c10b716b194e91269ac4150dc78b4c16e2b
elijah84 <assefa.belaye@gmail.com>
2017-10-11 16:55:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/home/phonemotiondetection1/AccelerometerListener.java
@@ -0,0 +1,14 @@
+package com.example.home.phonemotiondetection1;
+
+/**
+ * Created by home on 9/29/17.
+ */
+
+public interface AccelerometerListener {
+
+    public void onAccelerationChanged(float x, float y, float z);
+
+    public void onShake(float force);
+
+}
+
